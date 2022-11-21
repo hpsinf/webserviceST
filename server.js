5a1d94183d7e0cedaddcb6687063fa4c3e3d93c4
Henrique Pinheiro <hps.inf@gmail.com>
2022-11-21 09:10:38 -0300

    ajuste removeHeader

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-     res.removeHeader('Access-Control-Allow-Origin')
+     //res.removeHeader('Access-Control-Allow-Origin')
      res.removeHeader('Access-Control-Allow-Methods')
      res.removeHeader('Access-Control-Allow-Headers')
      res.removeHeader('Access-Control-Allow-Credentials')
